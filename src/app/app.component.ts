e45a062ac3f4734f6f486aede43803256f6876e5
Firas <firas.abedelnabi@gmail.com>
2021-03-04 17:39:31 +0100

    refactor: clears html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'aln-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'alten-app';
