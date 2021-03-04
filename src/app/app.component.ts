652ca5301ff684827867e7acc90bfd68b5893cb9
Firas AEN <FirasABEDELNABI@eaton.com>
2021-03-04 10:49:58 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aln-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'alten-app';
+}
