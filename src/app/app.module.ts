483fac375ef5cc96bb5f14a3de14a16a2f9cf6e9
Firas <firas.abedelnabi@gmail.com>
2021-03-04 18:15:39 +0100

    feat: adds book list and book item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NzAntModule} from './nz-ant/nz-ant.module';
+import {BooksExplorerModule} from './books-explorer/books-explorer.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import {NzAntModule} from './nz-ant/nz-ant.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BooksExplorerModule,
     NzAntModule,
   ],
   providers: [],
