a0ae72d67b42315883e4a41bc1476fd1f0bfdf4d
Firas <firas.abedelnabi@gmail.com>
2021-03-04 14:51:14 +0100

    feat: creates a standalone nz-ant module to import and export 3rd party library components

---
+++ src/app/nz-ant/nz-ant.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    NzButtonModule,
+  ],
+  exports: [
+    NzButtonModule,
+  ],
+})
+export class NzAntModule { }
